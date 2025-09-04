46a116089644598723a2c551a1dbed52c5dd6951
sahilsapariya <sahilsapariya03@gmail.com>
2025-09-05 00:24:23 +0530

    feat: added json converter

--- src/main/java/com/example/calfolio/entity/Calculator.java
+++ src/main/java/com/example/calfolio/entity/Calculator.java
@@ -2,6 +2,7 @@ package com.example.calfolio.entity;
 
 import java.util.Map;
 
+import com.example.calfolio.utils.JsonConverter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.*;
@@ -28,6 +29,7 @@ public class Calculator {
 
     @Column(columnDefinition = "json")
     @JsonProperty("calc_formula")
+    @Convert(converter = JsonConverter.class)
     private Map<String, Object> calcFormula;    
 
     @Column(name = "status")
---
+++ src/main/java/com/example/calfolio/utils/JsonConverter.java
@@ -0,0 +1,35 @@
+package com.example.calfolio.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Converter(autoApply = false)
+public class JsonConverter implements AttributeConverter<Map<String, Object>, String> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> attribute) {
+        if (attribute == null) return null;
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error converting Map to JSON", e);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> convertToEntityAttribute(String dbData) {
+        if (dbData == null) return new HashMap<>();
+        try {
+            return objectMapper.readValue(dbData, Map.class);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Error reading JSON from DB", e);
+        }
+    }
+}
